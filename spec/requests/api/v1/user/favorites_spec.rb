66ffe870bd3c1d022257f7ef9f031e9cc67b4431
Joe Jobes <jmrjobes@gmail.com>
2018-04-11 11:15:12 -0600

    Add request spec for post

--- spec/requests/api/v1/user/favorites_spec.rb
+++ spec/requests/api/v1/user/favorites_spec.rb
@@ -17,4 +17,20 @@ describe "Favorites Request Spec" do
     })
     expect(res.first[:neo_reference_id]).to eq("2153306")
   end
+
+  it "can post to favorites" do
+    user = create(:user)
+    api_key = create(:api_key)
+    post "/api/v1/user/favorites?api_key=#{api_key.value}&neo_reference_id=2021277"
+
+    expect(response).to be_success
+
+    res = JSON.parse(response.body, symbolize_names: true)
+
+    expect(res.first[:asteroid]).to eq({
+      name: "21277 (1996 TO5)",
+      is_potentially_hazardous_asteroid: false,
+    })
+    expect(res.first[:neo_reference_id]).to eq("2021277")
+  end
 end
