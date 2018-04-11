69d0ba7565a4145d54063037bbf526c8d478f893
Joe Jobes <jmrjobes@gmail.com>
2018-04-11 12:09:05 -0600

    delete extraneous methods in controller

--- app/controllers/most_dangerous_day_controller.rb
+++ app/controllers/most_dangerous_day_controller.rb
@@ -1,24 +1,5 @@
 class MostDangerousDayController < ApplicationController
   def index
-    # ain't this just an ugly bag of worms...
     @info = MostDangerousDayPresenter.new(params[:start_date], params[:end_date])
-  end
-
-  def find_all_dangerous_asteroids(roids)
-    roids.find_all do |roid|
-      roid.potentially_hazardous == true
-    end
-  end
-
-  def group_asteroids(asteroids)
-    asteroids.group_by do |asteroid|
-      asteroid.date
-    end
-  end
-
-  def day_with_most_dangerous_asteroids(asteroids)
-    asteroids.max_by do |key, value|
-      value.count
-    end
   end
 end
